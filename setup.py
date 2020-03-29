f5bd85906717eb58b8413fedfb43062104ec6887
Leandro B Pereira <leandrobatistapereira98@gmail.com>
2020-03-28 21:21:51 -0300

    Pacote criado e enviado ao Pypi

--- setup.py
+++ setup.py
@@ -101,7 +101,7 @@ def find_package_data(
     return out
 
 
-PACKAGE = "libpythonpro"
+PACKAGE = "libpythonpro_leandro"
 NAME = PACKAGE
 DESCRIPTION = "Módulo para exemplificar construção de projetos Python no curso PyTools"
 AUTHOR = "Leandro Batista Pereira"
